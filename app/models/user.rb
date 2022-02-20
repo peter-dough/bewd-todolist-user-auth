c2faacf823be0e2e683fb32ca9d03af3094fea16
peter-dough <pdo@apartmentlist.com>
2022-02-19 16:45:38 -0800

    Add pw encryption/decryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 64 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 } 
   validates_uniqueness_of :username
+  
+  after_validation :hash_password
 
   has_many :sessions
   has_many :tasks
+  
+  private
 
+  def hash_password
+    self.password = BCrypt::Password.create(self.password)
+  end
 end
